921dacdbad13016c73b208ec54d4966168a83f5c
Richard Wilson <richardmwilson191@gmail.com>
2020-03-15 12:13:18 +0000

    Updates

---
+++ precache-manifest.6ed00e7ddd187f45edfe245de83e191c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b75799ad6512e648fc4c75c82d6dd08",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3fe11cd1c9e2692fc0ef",
+    "url": "/monsters-rolodex/static/css/main.ed41bd29.chunk.css"
+  },
+  {
+    "revision": "0d0ff1038fb1e7a453a9",
+    "url": "/monsters-rolodex/static/js/2.4aaf030a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.4aaf030a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fe11cd1c9e2692fc0ef",
+    "url": "/monsters-rolodex/static/js/main.9860d575.chunk.js"
+  },
+  {
+    "revision": "bf0efc50803a2c0daead",
+    "url": "/monsters-rolodex/static/js/runtime-main.e47bae1e.js"
+  }
+]);
